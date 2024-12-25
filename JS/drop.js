4772deb3189c68a89aa0821dcf601593db7f46a5
Leon7065 <lonilee29@gmail.com>
2024-12-26 00:57:44 +0100

    help

--- JS/drop.js
+++ JS/drop.js
@@ -1,25 +1,34 @@
+
 const selectBtn = document.querySelector(".select-btn"),
-items = document.querySelectorAll(".item");
+    items = document.querySelectorAll(".item");
 
-console.log(selectBtn,items);
+console.log(selectBtn, items);
 
-selectBtn.addEventListener("click", () =>{
-    selectBtn.classList.toggle("open"); 
+selectBtn.addEventListener("click", () => {
+    selectBtn.classList.toggle("open");
 });
 
-items.forEach(item =>{
-    item.addEventListener("click", () =>{
+items.forEach(item => {
+    item.addEventListener("click", () => {
         item.classList.toggle("checked");
-    
-        let checked = document.querySelectorAll(".checked"),
-        btnText = document.querySelector(".btn-text");
 
-        if(checked && checked.length > 0){
+        let checked = document.querySelectorAll(".checked"),
+            btnText = document.querySelector(".btn-text");
+
+        if (checked && checked.length > 0) {
             btnText.innerText = `${checked.length} Selected`;
-        }else{
+        } else {
             btnText.innerText = "Select Language";
         }
     });
-})
+});
 
+// Function to confirm and hide the container if any items are checked
+function confirm() {
+    const container = document.querySelector('.container');
+    const checked = document.querySelectorAll(".checked");
 
+    if (checked && checked.length > 0) {
+        container.style.display = "none";
+    }
+}
--- JS/script.js
+++ JS/script.js
@@ -7,3 +7,29 @@ function nextStep(step) {
 
     document.getElementById(`step${step + 1}`).classList.add("active");
 }
+
+// ================================= drop select ==============================
+
+const selectBtn = document.querySelector(".select-btn"),
+items = document.querySelectorAll(".item");
+
+console.log(selectBtn,items);
+
+selectBtn.addEventListener("click", () =>{
+    selectBtn.classList.toggle("open"); 
+});
+
+items.forEach(item =>{
+    item.addEventListener("click", () =>{
+        item.classList.toggle("checked");
+    
+        let checked = document.querySelectorAll(".checked"),
+        btnText = document.querySelector(".btn-text");
+
+        if(checked && checked.length > 0){
+            btnText.innerText = `${checked.length} Selected`;
+        }else{
+            btnText.innerText = "Select Language";
+        }
+    });
+})
