889c4b99f8d2c9d10fae87246b8ddbb4eb33ae01
Leon7065 <lonilee29@gmail.com>
2024-12-25 22:01:38 +0100

    sing up form in progress

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,9 @@
-let currentStep = 0;
-
 function nextStep(step) {
+    // if(document.getElementById('input-header').value === null){
+
+    // }
+    //krijo validimin
     document.getElementById(`step${step}`).classList.remove("active");
+
     document.getElementById(`step${step + 1}`).classList.add("active");
 }
