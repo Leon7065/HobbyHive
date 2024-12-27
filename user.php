f90116289d7fb63c182621e4fca3876f6d6a1fc1
Leon7065 <lonilee29@gmail.com>
2024-12-27 23:17:14 +0100

    hmmm

--- user.php
+++ user.php
@@ -6,6 +6,7 @@
     <title>Username</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" >
     <link rel="stylesheet" href="CSS/user.css">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
     <style>
         .posts{
             display: grid;
@@ -58,18 +59,15 @@
             <li><strong>15</strong> Following</li>
             <li><strong>15</strong> Followers</li>
         </ul>
-        <hr>
+        <hr>    
 
         <div class="posts">
-            <img src="images/Jon_Snow.png" alt="">
-            <img src="images/Jon_Snow.png" alt="">
-            <img src="images/Jon_Snow.png" alt="">
+           
             <img src="images/Jon_Snow.png" alt="">
             <img src="images/Jon_Snow.png" alt="">
             <img src="images/Jon_Snow.png" alt="">
             <img src="images/Jon_Snow.png" alt="">
         </div>
-
-    </div>
+    </div> 
 </body>
 </html>
