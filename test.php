12a05eaf4c2cc0f1ca6bef5f77bd337d6928a684
Leon7065 <lonilee29@gmail.com>
2024-12-30 18:24:48 +0100

    preview..

--- test.php
+++ test.php
@@ -7,46 +7,98 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" >
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="CSS/test.css">
-  </head>
+    
+</head>
 <body>
+
+    <!-- if there is no image: description and under that comments -->
+
     <div class="preview">
         <div class="post-image">
             <img src="images/dostikaftjall.png" alt="Post Image" class="post-img">
-            <hr>
+            <!-- <hr> -->
             <p class="description">lorem llapallapallapallapallapa</p>
         </div>
 
         <div class="post-content">
             <div class="post-profile">
                 <div class="header">
-                    <img src="" alt="Profile picture">
+                    <img src="images/dog.jpg" class="pfp" alt="Profile picture">
                     <strong>Username</strong>
                </div>
                <i class="fa-solid fa-ellipsis"></i>
             </div>
-            <hr>
+
+            <hr style="margin: 2px;">
+
             <div class="comments">
                 <div class="comment">
-
-                    <img src="" alt="Profile picture">
-
-                    <div class="comment-content">
-
+                    <div class="comment-content" >
                         <div class="comment-text">
-                            <p><strong>Username</strong>description</p>
-                            <i class="fa-regular fa-heart"></i>
+                            <img src="images/dog.jpg" class="pfp" alt="Profile picture">
+                            <p><strong>Username</strong> Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque laborum culpa autem adipisci quasi accusamus nam inventore consectetur illo praesentium! Earum provident nulla harum voluptatum voluptas cupiditate asperiores dicta culpa.</p>
                         </div>
+                        <i class="fa-regular fa-heart" style="transform: translateY(10px);"></i>
+                    </div>
 
-                        <div class="comment-interactions">
-                            <p>timestamp</p>
-                            <p>likes</p>
-                            <p>reply</p>
-                        </div>
+                    <div class="comment-interactions">
+                        <p>timestamp</p>
+                        <p>likes</p>
+                        <p>reply</p>
+                       <i class="fa-solid fa-ellipsis" style="transform: translateY(4px);"></i>
                     </div>
                 </div>
-             </div>
-           </div>
-      </div>
+                <div class="comment">
+                    <div class="comment-content" >
+                        <div class="comment-text">
+                            <img src="images/dog.jpg" class="pfp" alt="Profile picture">
+                            <p><strong>Username</strong> Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque laborum culpa autem adipisci quasi accusamus nam inventore consectetur illo praesentium! Earum provident nulla harum voluptatum voluptas cupiditate asperiores dicta culpa.</p>
+                        </div>
+                        <i class="fa-regular fa-heart" style="transform: translateY(10px);"></i>
+                    </div>
+
+                    <div class="comment-interactions">
+                        <p>timestamp</p>
+                        <p>likes</p>
+                        <p>reply</p>
+                       <i class="fa-solid fa-ellipsis" style="transform: translateY(4px);"></i>
+                    </div>
+                </div>
+                <div class="comment">
+                    <div class="comment-content" >
+                        <div class="comment-text">
+                            <img src="images/dog.jpg" class="pfp" alt="Profile picture">
+                            <p><strong>Username</strong> Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque laborum culpa autem adipisci quasi accusamus nam inventore consectetur illo praesentium! Earum provident nulla harum voluptatum voluptas cupiditate asperiores dicta culpa.</p>
+                        </div>
+                        <i class="fa-regular fa-heart" style="transform: translateY(10px);"></i>
+                    </div>
+
+                    <div class="comment-interactions">
+                        <p>timestamp</p>
+                        <p>likes</p>
+                        <p>reply</p>
+                       <i class="fa-solid fa-ellipsis" style="transform: translateY(4px);"></i>
+                    </div>
+                </div>
+                
+
+            </div>
+
+            <hr style="margin: 2px;">
+
+            <div class="interactivity">
+                <div class="like-section">
+                    <i class="fa-regular fa-heart" id="like" ></i>
+                    <p>Like?</p>
+                </div>    
+                <form action="" class="add-comment">
+                    <input type="text" placeholder="Add a comment...">
+                    <button type="submit">post</button>
+                </form>
+            </div>
+
+        </div>
+    </div>
 <!-- 
 ======================== ADD INTERACTIVITY AND "ADD COMMENT"      
 -->
